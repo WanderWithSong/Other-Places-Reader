48d1fee54c7ec82d7c7cf9a667c5e80df948f211
Murphy Foxy <wanderwithjysong@gmail.com>
2020-06-14 00:12:35 -0400

    sidebar

--- resources/aby-script.js
+++ resources/aby-script.js
@@ -4,3 +4,30 @@ let hideNotice = function(){
     notice.style.display='none';
 };
 close.onclick = hideNotice;
+
+
+let bookmark = document.getElementById('side-home-bookmark');
+let sideHome = document.getElementById('side-home');
+let sideHomeStatus = true;
+
+/*
+bookmark.onclick = ()=> {
+    if(sideHomeStatus === true){
+      sideHomeStatus = false;
+      sideHome.style.right =`${-360}px`;
+   } else if(sideHomeStatus === false){
+       sideHomeStatus = true;
+       sideHome.style.right =`${0}px`;
+   }
+}
+*/
+
+
+/*
+window.onscroll = function(){scrollFunction()};
+function scrollFunction() {
+    if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        sideHome.style.display = 'none';
+    }
+}
+*/
